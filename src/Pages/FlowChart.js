a60010511caebcaadc9b97661ee17207e6b13283
lfvg <lfvg@cin.ufpe.br>
2020-07-14 14:33:04 -0300

    Fuxograma sem callback

--- src/Pages/FlowChart.js
+++ src/Pages/FlowChart.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Component} from 'react';
+import { Component } from 'react';
 import './FlowChart.css';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button'
 
-import { FlowChartWithState, FlowChart, NodeDefault, PortDefault, PortDefaultOuter, PortsDefault, ICanvasOuterDefaultProps} from "@mrblenny/react-flow-chart";
+import { FlowChartWithState, FlowChart, NodeDefault, PortDefault, PortDefaultOuter, PortsDefault, ICanvasOuterDefaultProps } from "@mrblenny/react-flow-chart";
 
 import VideocamRounded from '@material-ui/icons/VideocamRounded';
 import MeetingRoomRoundedIcon from '@material-ui/icons/MeetingRoomRounded';
@@ -13,15 +13,15 @@ import SettingsRoundedIcon from '@material-ui/icons/SettingsRounded';
 import VolumeUpRoundedIcon from '@material-ui/icons/VolumeUpRounded';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faVideo, faDoorOpen, faCogs, faBullhorn} from '@fortawesome/free-solid-svg-icons';
+import { faVideo, faDoorOpen, faCogs, faBullhorn } from '@fortawesome/free-solid-svg-icons';
 
 
 import * as actions from "@mrblenny/react-flow-chart/src/container/actions";
 
 import { SidebarItem } from '../components/SidebarItem'
-import {Page} from '../components/Page';
+import { Page } from '../components/Page';
 import CustomCanvas from '../components/Canvas';
-import Sidebar from  '../components/Sidebar';
+import Sidebar from '../components/Sidebar';
 import Port from '../components/Port';
 import Content from '../components/Content';
 import Message from '../components/Message';
@@ -31,11 +31,11 @@ import NodeInner from '../components/NodeInner';
 
 const NodeInnerCustom = ({ node, config }) => {
   return (
-    <Grid  container alignItems='center' justify='center'>
-      <Grid item justify='center' style={{height:49, color: '#F0F', padding:5, border: '2px #3366FC', borderRadius: 50, borderStyle: 'solid dotted solid solid'}}>
-        <VideocamRounded fontSize='large' style={{color: '#FFF', backgroundColor: '#3366FC', borderRadius: 50, padding: 8}}/>
+    <Grid container alignItems='center' justify='center'>
+      <Grid item justify='center' style={{ height: 49, color: '#F0F', padding: 5, border: '2px #3366FC', borderRadius: 50, borderStyle: 'solid dotted solid solid' }}>
+        <VideocamRounded fontSize='large' style={{ color: '#FFF', backgroundColor: '#3366FC', borderRadius: 50, padding: 8 }} />
       </Grid>
-      <Grid item> 
+      <Grid item>
         <Typography>{node.name}</Typography>
       </Grid>
     </Grid>
@@ -56,284 +56,297 @@ const stateActionCallbacks = Object.keys(actions).reduce((obj, key, idx) => {
   return obj;
 }, {});
 
-class Teste extends Component{
-  render(){
-    return(
-      
-      <Grid  container alignItems='center' justify='center'>
-        <Grid item justify='center' style={{height:49, padding:5, border: '2px #3366FC', borderRadius: 50, borderStyle: 'solid dotted solid solid'}}>
-        {
-          this.props.node.properties.icon === 'camera' &&
-          <div style={{color: '#FFF', backgroundColor: '#3366FC', borderRadius: 50, padding: 8}}>
-            <FontAwesomeIcon icon={faVideo}/>
-          </div>
-        }
-        {
-           this.props.node.properties.icon === 'room' &&
-            <div style={{color: '#FFF', backgroundColor: '#3366FC', borderRadius: 50, padding: 8}}>
-              <FontAwesomeIcon icon={faDoorOpen}/>
+class Teste extends Component {
+  render() {
+    return (
+
+      <Grid container alignItems='center' justify='center'>
+        <Grid item justify='center' style={{ height: 49, padding: 5, border: '2px #3366FC', borderRadius: 50, borderStyle: 'solid dotted solid solid' }}>
+          {
+            this.props.node.properties.icon === 'camera' &&
+            <div style={{ color: '#FFF', backgroundColor: '#3366FC', borderRadius: 50, padding: 8 }}>
+              <FontAwesomeIcon icon={faVideo} />
             </div>
-        }
-        {
-           this.props.node.properties.icon === 'analytics' &&
-           <div style={{color: '#FFF', backgroundColor: '#3366FC', borderRadius: 50, padding: 8}}>
-            <FontAwesomeIcon icon={faCogs}/>
-          </div>
-        }
-        {
-           this.props.node.properties.icon === 'actions' &&
-           <div style={{color: '#FFF', backgroundColor: '#3366FC', borderRadius: 50, padding: 8}}>
-            <FontAwesomeIcon icon={faBullhorn}/>
-          </div>
-        }
+          }
+          {
+            this.props.node.properties.icon === 'room' &&
+            <div style={{ color: '#FFF', backgroundColor: '#3366FC', borderRadius: 50, padding: 8 }}>
+              <FontAwesomeIcon icon={faDoorOpen} />
+            </div>
+          }
+          {
+            this.props.node.properties.icon === 'analytics' &&
+            <div style={{ color: '#FFF', backgroundColor: '#3366FC', borderRadius: 50, padding: 8 }}>
+              <FontAwesomeIcon icon={faCogs} />
+            </div>
+          }
+          {
+            this.props.node.properties.icon === 'actions' &&
+            <div style={{ color: '#FFF', backgroundColor: '#3366FC', borderRadius: 50, padding: 8 }}>
+              <FontAwesomeIcon icon={faBullhorn} />
+            </div>
+          }
         </Grid>
-        <Grid item> 
+        <Grid item>
           <Typography>{this.props.node.name}</Typography>
         </Grid>
       </Grid>
     )
   }
 }
-    class Main extends Component{
+class Main extends Component {
 
-        constructor(props){
-            super(props);
-    
-            this.state = {
-                  
-                
-                chartSimple : {
-                  //scale buga o setup inicial de adicionar nós e links
-                    scale: 0.9,
-                    offset: {
-                      x: 1,
-                      y: 1
-                    },
-                    nodes: {
-                      node1: {
-                        name: "Teste de nome",
-                        id: "node1",
-                        type: "input-output",
-                    
-                        position: {
-                          x: 300,
-                          y: 100
-                        },
-                        ports: {
-                          port1: {
-                            id: "port1",
-                            type: "right",
-                            
-                          },
-                          port2: {
-                            id: "port2",
-                            type: "left",
-                           
-                          }
-                        },
-                        properties:{
-                          icon: 'camera'
-                        }
-                      },
-                      node2: {
-                        name: "Nome do segundo nó",
-                        id: "node2",
-                        type: "input-output",
-                        icon: 'camera',
-                        position: {
-                          x: 300,
-                          y: 300
-                        },
-                        ports: {
-                          port1: {
-                            id: "port1",
-                            type: "left"
-                          },
-                          port2: {
-                            id: "port2",
-                            type: "right"
-                          }
-                        },
-                        properties:{
-                          icon: 'camera'
-                        }
-                      },
-                      node3: {
-                        name: "Ultimo nome",
-                        id: 'node3',
-                        type: 'input-output',
-                        icon: 'room',
-                        position: {
-                          x: 0,
-                          y: 0
-                        },
-                        ports:{
-                          port1:{
-                            id:'port1',
-                            type:'right'
-                          }, 
-                          port2:{
-                              id:'port2',
-                              type:'left'
-                          }
-                        },
-                        properties:{
-                          icon: 'room'
-                        }
-                      }
-                    },
-                    links: {
-                      link1: {
-                        id: "link1",
-                        from: {
-                          nodeId: "node1",
-                          portId: "port2"
-                        },
-                        to: {
-                          nodeId: "node2",
-                          portId: "port1"
-                        },
-                      },
-                    },
-                    selected: {},
-                    hovered: {}
-                },
-            
+  constructor(props) {
+    super(props);
 
-            }
-        }
-        
-        render(){
-        return(
- 
-    <Page>
-      <Content>
-        <FlowChartWithState 
-            initialValue={this.state.chartSimple} 
-            config={{snapToGrid: false , selectable: false, readonly: !this.state.eddit, smartRouting: false, zoom:{minScale: 0.9, maxScale:1.3}}}
-            Components={{CanvasOuter: CustomCanvas, NodeInner: Teste, Node: Node, Port: Port}}
-            callbacks={stateActionCallbacks}
-           />
-      </Content>
-      <Sidebar>
-        <Message>
-            Build 19041.329
-            SENAI Confidential 
-        </Message>
-      
-        <SidebarItem 
-          type='left-right'
-          ports={ {
-            port1: {
-              id: 'port1',
-              type: 'left',
-              properties: {
-                custom: 'property',
-              },
+    this.state = {
+
+
+      chartSimple: {
+        //scale buga o setup inicial de adicionar nós e links
+        scale: 0.9,
+        offset: {
+          x: 1,
+          y: 1
+        },
+        nodes: {
+          node1: {
+            name: "Teste de nome",
+            id: "node1",
+            type: "input-output",
+
+            position: {
+              x: 300,
+              y: 100
             },
-            port2: {
-              id: 'port2',
-              type: 'right',
-              properties: {
-                custom: 'property',
+            ports: {
+              port1: {
+                id: "port1",
+                type: "right",
+
               },
+              port2: {
+                id: "port2",
+                type: "left",
+
+              }
             },
-          }}
-          properties={{
-            icon: 'room',
-            name: "Room"
-          }}
-        />
-          <SidebarItem
-          type="left-right"
-          ports={{
-          port1: {
-            id: 'port1',
-            type: 'left',
             properties: {
-              custom: 'property',
               icon: 'camera'
-            },
+            }
           },
-          port2: {
-            id: 'port2',
-            type: 'right',
-            properties: {
-              custom: 'property',
-            },
-          },
-          }}
-          properties={{
+          node2: {
+            name: "Nome do segundo nó",
+            id: "node2",
+            type: "input-output",
             icon: 'camera',
-            name: 'Camera'
-          }}
+            position: {
+              x: 300,
+              y: 300
+            },
+            ports: {
+              port1: {
+                id: "port1",
+                type: "left"
+              },
+              port2: {
+                id: "port2",
+                type: "right"
+              }
+            },
+            properties: {
+              icon: 'camera'
+            }
+          },
+          node3: {
+            name: "Ultimo nome",
+            id: 'node3',
+            type: 'input-output',
+            icon: 'room',
+            position: {
+              x: 0,
+              y: 0
+            },
+            ports: {
+              port1: {
+                id: 'port1',
+                type: 'right'
+              },
+              port2: {
+                id: 'port2',
+                type: 'left'
+              }
+            },
+            properties: {
+              icon: 'room'
+            }
+          }
+        },
+        links: {
+          link1: {
+            id: "link1",
+            from: {
+              nodeId: "node1",
+              portId: "port2"
+            },
+            to: {
+              nodeId: "node2",
+              portId: "port1"
+            },
+          },
+        },
+        selected: {},
+        hovered: {}
+      },
+
+
+    }
+  }
+
+  render() {
+    const stateActionCallbacks = Object.keys(actions).reduce((obj, key, idx) => {
+      obj[key] = (...args) => {
+        let action = actions[key];
+        let newChartTransformer = action(...args);
+        let newChart = newChartTransformer(this.state.chartSimple);
+        this.setState({
+          chartSimple: { ...this.state.chartSimple, ...newChart }
+        });
+        return newChart;
+      };
+      return obj;
+    }, {});
+    return (
+
+      <Page>
+        <Content>
+          <FlowChartWithState
+            initialValue={this.state.chartSimple}
+            config={{ snapToGrid: false, selectable: false, readonly: !this.state.eddit, smartRouting: false, zoom: { minScale: 0.9, maxScale: 1.3 } }}
+            Components={{ CanvasOuter: CustomCanvas, NodeInner: Teste, Node: Node, Port: Port }}
+            callbacks={stateActionCallbacks}
           />
-        <SidebarItem 
-          type='left-right'
-          ports={ {
-            port1: {
-              id: 'port1',
-              type: 'left',
-              properties: {
-                custom: 'property',
+        </Content>
+        <Sidebar>
+          <Message>
+            Build 19041.329
+            SENAI Confidential
+        </Message>
+
+          <SidebarItem
+            type='left-right'
+            ports={{
+              port1: {
+                id: 'port1',
+                type: 'left',
+                properties: {
+                  custom: 'property',
+                },
               },
-            },
-            port2: {
-              id: 'port2',
-              type: 'right',
-              properties: {
-                custom: 'property',
+              port2: {
+                id: 'port2',
+                type: 'right',
+                properties: {
+                  custom: 'property',
+                },
               },
-            },
-          }}
-          properties={{
-            icon: 'analytics',
-            name: 'Analytics'
-          }}
-        />
-        <SidebarItem
-          type='left-right'
-          ports={ {
-            port1: {
-              id: 'port1',
-              type: 'left',
-              properties: {
-                custom: 'property',
+            }}
+            properties={{
+              icon: 'room',
+              name: "Room"
+            }}
+          />
+          <SidebarItem
+            type="left-right"
+            ports={{
+              port1: {
+                id: 'port1',
+                type: 'left',
+                properties: {
+                  custom: 'property',
+                  icon: 'camera'
+                },
               },
-            },
-            port2: {
-              id: 'port2',
-              type: 'right',
-              properties: {
-                custom: 'property',
+              port2: {
+                id: 'port2',
+                type: 'right',
+                properties: {
+                  custom: 'property',
+                },
               },
-            },
-          }}
-          properties={{
-            icon: 'actions',
-            name: 'Actions'
-          }}
+            }}
+            properties={{
+              icon: 'camera',
+              name: 'Camera'
+            }}
+          />
+          <SidebarItem
+            type='left-right'
+            ports={{
+              port1: {
+                id: 'port1',
+                type: 'left',
+                properties: {
+                  custom: 'property',
+                },
+              },
+              port2: {
+                id: 'port2',
+                type: 'right',
+                properties: {
+                  custom: 'property',
+                },
+              },
+            }}
+            properties={{
+              icon: 'analytics',
+              name: 'Analytics'
+            }}
+          />
+          <SidebarItem
+            type='left-right'
+            ports={{
+              port1: {
+                id: 'port1',
+                type: 'left',
+                properties: {
+                  custom: 'property',
+                },
+              },
+              port2: {
+                id: 'port2',
+                type: 'right',
+                properties: {
+                  custom: 'property',
+                },
+              },
+            }}
+            properties={{
+              icon: 'actions',
+              name: 'Actions'
+            }}
           />
           {
-              !this.state.eddit ? (
-                <Button onClick={event =>{
-                  let chart  = this.state.chartSimple;
-                  chart.scale = 1.0;
-                  
-                  this.setState({eddit: true, chartSimple: chart})}
-                }>Editar</Button>
-              ) : (
-                  <div/>
+            !this.state.eddit ? (
+              <Button onClick={event => {
+                let chart = this.state.chartSimple;
+                chart.scale = 1.0;
+
+                this.setState({ eddit: true, chartSimple: chart })
+              }
+              }>Editar</Button>
+            ) : (
+                <div />
               )
           }
-        <Button onClick={event => 
+          <Button onClick={event =>
             console.log(this.state)
           }>Print States</Button>
-          
-      </Sidebar>
-    </Page>
-            )
-    }
+
+        </Sidebar>
+      </Page>
+    )
+  }
 }
- 
+
 export default Main;
--- src/components/SidebarItem.js
+++ src/components/SidebarItem.js
@@ -3,15 +3,11 @@ import styled from 'styled-components'
 import { INode, REACT_FLOW_CHART } from '@mrblenny/react-flow-chart'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography';
-
-import VideocamRounded from '@material-ui/icons/VideocamRounded';
+/** */
 import DragIndicatorIcon from '@material-ui/icons/DragIndicator';
-import MeetingRoomRoundedIcon from '@material-ui/icons/MeetingRoomRounded';
-import SettingsRoundedIcon from '@material-ui/icons/SettingsRounded';
-import VolumeUpRoundedIcon from '@material-ui/icons/VolumeUpRounded';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faVideo, faDoorOpen, faCogs, faBullhorn, faEllipsisV} from '@fortawesome/free-solid-svg-icons';
+import { faVideo, faDoorOpen, faCogs, faBullhorn, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
 const Outer = styled.div`
   padding: 1px;
@@ -27,20 +23,20 @@ const Outer = styled.div`
 `
 
 
-export const SidebarItem = ({type, icon, ports, properties}) => {
+export const SidebarItem = ({ type, icon, ports, properties }) => {
   return (
     <Outer
       draggable={true}
-      onDragStart={ (event) => {
-        
-        event.dataTransfer.setData(REACT_FLOW_CHART, JSON.stringify({type, icon, ports, properties}))
+      onDragStart={(event) => {
+
+        event.dataTransfer.setData(REACT_FLOW_CHART, JSON.stringify({ type, icon, ports, properties }))
       }}
     >
       <Grid container direction='row' alignItems='center'>
-        <Grid  style={{width: '20%', padding:2}}>
-          <DragIndicatorIcon style={{color: '#3366FC', marginTop: 3}}/>
+        <Grid style={{ width: '20%', padding: 2 }}>
+          <DragIndicatorIcon style={{ color: '#3366FC', marginTop: 3 }} />
         </Grid>
-        <Grid container  direction='row' alignItems='center' style={{width: '80%', background:'#3366FC', padding:2, paddingLeft:8, borderRadius: '0px 3px 3px 0px', color: '#FFF'}}>
+        <Grid container direction='row' alignItems='center' style={{ width: '80%', background: '#3366FC', padding: 2, paddingLeft: 8, borderRadius: '0px 3px 3px 0px', color: '#FFF' }}>
           {
             properties.icon === 'camera' && <FontAwesomeIcon icon={faVideo} size='2x'/>
           }
@@ -52,8 +48,8 @@ export const SidebarItem = ({type, icon, ports, properties}) => {
           }
           {
             properties.icon === 'actions' && <FontAwesomeIcon icon={faBullhorn} size='2x'/>
-          } 
-          <Typography style={{marginLeft: 12}} variant='body2'>{properties.name}</Typography>
+          }
+          <Typography style={{ marginLeft: 12 }} variant='body2'>{properties.name}</Typography>
         </Grid>
       </Grid>
       {/*type*/}
